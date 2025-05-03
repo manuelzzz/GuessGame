8aedfd3674494eb671aab42d553ef5efaf1a8223
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2025-05-03 11:26:52 -0300

    Initial Commit

---
+++ GuessApp/GuessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessAppApp.swift
+//  GuessApp
+//
+//  Created by Manuel Santos Souza on 03/05/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
